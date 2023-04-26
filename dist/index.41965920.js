424c23e983d79512edb20da1003f610c3ae87309
KRISTIAN DEL BARRIO <kdelbarrio@MacBook-Air-de-KRISTIAN.local>
2023-04-26 22:32:22 +0200

    segundo commit

---
+++ dist/index.41965920.js
@@ -0,0 +1,2 @@
+!function(){const e=document.getElementById("button-menu"),t=document.getElementById("nav");e.addEventListener("click",(()=>{e.classList.toggle("close"),t.classList.toggle("show")})),t.addEventListener("click",(s=>{"nav"===s.target.id&&(t.classList.remove("show"),e.classList.remove("close"))}))}();
+//# sourceMappingURL=index.41965920.js.map
