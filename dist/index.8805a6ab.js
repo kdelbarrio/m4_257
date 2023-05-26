38f3734074dea9e9de915f158379bdf1c57d7f36
KRISTIAN DEL BARRIO <kdelbarrio@MacBook-Air-de-KRISTIAN.local>
2023-05-26 20:48:46 +0200

    Pec 2

--- dist/index.8805a6ab.js
+++
@@ -1,2 +0,0 @@
-const e=document.getElementById("button-menu"),t=document.getElementById("nav");e.addEventListener("click",(()=>{e.classList.toggle("close"),t.classList.toggle("show")})),t.addEventListener("click",(s=>{"nav"===s.target.id&&(t.classList.remove("show"),e.classList.remove("close"))}));
-//# sourceMappingURL=index.8805a6ab.js.map
